7f5c0a2367190a7685aa96819c5b1edf5457a7d8
Matthew Rothenberg <mrothenberg@gmail.com>
2011-12-03 13:36:31 -0500

    initial generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personalappeal::Application.config.secret_token = 'ee3f5e11c7bc12fc7d07e95444c2a12a176b823b3a6b11b229413ef327c2d4a57fe84287ecada3a73c1b2f3caf8d78b093c31cb4b367cbdc66b6c3c04a6a3563'
