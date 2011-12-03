323e3f2dbf751dce4ed8627b2e489b78c7595820
Matthew Rothenberg <mrothenberg@gmail.com>
2011-12-03 14:08:52 -0500

    seed values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Contender.create( [
+                    {uname: 'Brandon',  fullname: 'Brandon Harris'},    #wikipedia programmer
+                    {uname: 'Isaac',    fullname: 'Isaac Kosegi'},      #wikipedia editor
+                    {uname: 'James',    fullname: 'Dr. James Heilman'}, #wikipedia editor
+                    {uname: 'Jimmy',    fullname: 'Jimmy Wales'},       #wikipedia founder
+                    {uname: 'Judy',     fullname: 'Judy Mollica'},      #wikipedia reader
+                    {uname: 'Karthik',  fullname: 'Karthik Nadar'},     #wikipedia author
+                    {uname: 'Ryan',     fullname: 'Ryan Lane'},         #wikipedia programmer
+                    {uname: 'Sarah',    fullname: 'Sarah'},             #author of 159 articles
+                    {uname: 'Stephen',  fullname: 'Stephen'},           #author of 191 articles
+                    {uname: 'Susan',    fullname: 'Susan'}              #author of 549 articles
+                  ]
+                )
