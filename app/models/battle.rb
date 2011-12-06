17b893db7384185485c8472477b85943e1f622fc
Matthew Rothenberg <mrothenberg@gmail.com>
2011-12-06 13:52:06 -0500

    more validation

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -2,7 +2,7 @@ class Battle < ActiveRecord::Base
   belongs_to :winner, :class_name => 'Contender', :foreign_key => "winner_id", :counter_cache => :wins
   belongs_to :loser, :class_name => 'Contender', :foreign_key => "loser_id", :counter_cache => :loses
   
-  validates :winner_id, :presence => true
-  validates :loser_id, :presence  => true
+  validates :winner_id, :presence => true, :numericality => { :only_integer => true }
+  validates :loser_id, :presence  => true, :numericality => { :only_integer => true }
   
 end
